d33baa1ae4006026cc5d55890caf2da2171373aa
Julien Franzi <julien-franzi@live.fr>
2023-02-15 15:53:35 +0100

    MAJ

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 // const Titre4= (props) => <h1 className="text-center">{props.content}</h1>
 const Product= (props) => (
     <div className="card text-center" style={{width:14+ 'em'}}>
-        <img src={props.details.Image} class="card-img-top w-75 h-75 mx-3" alt="..."></img>
+        <img src={props.details.Image} className="card-img-top w-75 h-75 mx-2"id="cardImg" alt="..."></img>
         <div className="card-body">
             <h5 className="card-title">{props.details.name}</h5>
             <p className="card-text">{props.details.price}€</p>
@@ -31,20 +31,51 @@ class App extends React.Component{
             {id: 6, name:"Beef BBQ", price: 10.95, Image:"https://risibank.fr/cache/medias/0/8/838/83884/full.png"},
             {id: 7, name:"Oreo", price: 1.95, Image:"https://risibank.fr/cache/medias/0/2/201/20132/full.jpeg"},
             {id: 8, name:"Menu BestOF", price: 15.95, Image:"https://risibank.fr/cache/medias/0/19/1983/198357/full.png"},
+            {id: 9, name:"Chicken BBQ", price: 10.95, Image:"image/Mon projet.png"},
+            {id: 10, name:"Burger Chicken", price: 7.95, Image:"image/SeekPng.com_burger-png_790996.png"},
+            {id: 11, name:"Bucket Chick", price: 20.95, Image:"image/Mon projet (1).png"},
+            {id: 12, name:"Nuggets Spicy", price: 9.95, Image:"image/Mon projet3.png"},
         ]
         //content:"Mon composant Class"
     }
-    handleClick=(e)=>console.log(e.target.parentNode)
+    handleClick=(e)=> {
+        const clickedElementId = e.target.parentNode.id // renvoie la div entière
+        const clicked = this.state.products.find(element = element.id == clickedElementId)
+        const copiedOrdered = [...this.state.ordered];
+        copiedOrdered.push(clicked)
+        this.setState({ordered: copiedOrdered})
+    }
     
     render(){
         const productsList = this.state.products.map(product => 
             <Product key={product.id} details={product} handleClick={this.handleClick}/>
         )
-        return <div className="list">{productsList}</div>
-        //<React.Fragment>
+        //const orderedList = this.state.ordered.map(order =>)
+
+        return (
+    <React.Fragment>
+    <ul id="menu-list" style={{
+    display:"none"    
+    }}>
+        <h1>Panier</h1>  
+        <li>Entrée</li>
+        <span className="Total">TOTAL :</span>
+        <button className="cta"id="pay">
+            <span class="span"><i className="fa-solid fa-credit-card"></i></span>
+        <div></div>   
+        </button>
+        <button className="cta"id="trash">
+            <span class="span"><i className="fa-solid fa-trash"></i></span>
+        </button>
+    </ul>
+        <div className="list">{productsList}
+        </div>
+        
+    </React.Fragment>
+    )
+        
             //<h1 className= "text-center">{this.state.content}</h1>
             //<button className="btn">Ma fonction</button>
-        //</React.Fragment>
         
         
     }
@@ -65,6 +96,17 @@ ReactDOM.render(
 );
 
 
+const menuBtn = document.querySelector('.cta');
+
+menuBtn.addEventListener('click', () => {
+    const menuList = document.querySelector('#menu-list');
+    console.log(menuList)
+    if (menuList.style.display === 'none') {
+    menuList.style.display = 'block';
+  } else {
+    menuList.style.display = 'none';
+  }
+});
 
 
 
